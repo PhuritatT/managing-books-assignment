95dbc6940d583a39cf9ed84d68645857befef983
PuritatT <67301841+PhuritatT@users.noreply.github.com>
2025-03-24 02:06:53 +0700

    Initial commit

---
+++ src/main/java/com/example/managing_books/ManagingBooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.managing_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/managing_books/ManagingBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managing_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
